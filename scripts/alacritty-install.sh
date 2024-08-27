639e6dcb66a8355ec271d834d28c6995f0ae38f4
Catalin Soare <stevensuncatalin369@gmail.com>
2024-08-28 00:42:38 +0300

    added install and uninstall scripts for the alacritty configuration

---
+++ scripts/alacritty-install.sh
@@ -0,0 +1,2 @@
+sudo ln alacritty.toml ~/.config/alacritty
+cp -r alacritty-theme ~/.config/alacritty/themes
---
+++ scripts/alacritty-uninstall.sh
@@ -0,0 +1,2 @@
+sudo rm -rf ~/.config/alacritty/alacritty.toml
+sudo rm -rf ~/.config/alacritty/themes/alacritty-theme
