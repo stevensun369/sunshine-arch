82ba305c4c8564a422257286d15df5c4e9ed7d32
Catalin Soare <stevensuncatalin369@gmail.com>
2024-08-28 00:32:25 +0300

    themed rofi

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *barlayout        = "tln|s";
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -113,4 +114,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
--- scripts/rofi-install.sh
+++ scripts/rofi-install.sh
@@ -1,5 +1,8 @@
 sudo ln rofi-run.sh ~/.
 
+# config
+sudo ln config.rasi ~/.config/rofi/.	
+
 # wifi menu
 git clone https://github.com/zbaylin/rofi-wifi-menu/
 
